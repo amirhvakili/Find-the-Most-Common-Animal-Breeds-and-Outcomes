390b7767f54dae5d38f9db88e3ad4976973ec826
unknown <amirhosein7210@gmail.com>
2026-05-18 21:44:01 +0330

    Third commit

--- dags/animals_pipeline_dags.py
+++ dags/animals_pipeline_dags.py
@@ -10,21 +10,16 @@ sys.path.insert(0, str(project_root))
 from scripts.telegram_alert import send_telegram_alert
 from scripts.extract import extract_function
 
-default_args = {
-    'owner': 'airflow',
-    'on_failure_callback': send_telegram_alert
-}
-
 
 with DAG(
     dag_id="animal_pipeline_dag",
     start_date=datetime(2024, 1, 1),
-    default_args=default_args,
+    default_args={'owner': 'airflow', 'on_failure_callback': send_telegram_alert},
     schedule=None,
     catchup=False
 ) as dag:
 
     extract_task = PythonOperator(
         task_id="extract",
-        python_callable=extract_function
+        python_callable=extract_function,
     )
